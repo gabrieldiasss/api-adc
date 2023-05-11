bac5068f3c62c75a1ee14ab99c4eca953d908e45
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2023-05-11 18:29:34 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Hello')
+import express from 'express'
+import { config } from 'dotenv'
+const app = express()
+
+const port = process.env.PORT || 8000
+
+app.get("/", (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(port, () => console.log(`listening on port ${port}`))
